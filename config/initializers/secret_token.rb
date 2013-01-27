01e9b62b77328f36d9aca9b03f856e42772cd6b6
Jake Siddall <jacobsiddall@gmail.com>
2013-01-27 16:56:23 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoMercyChess::Application.config.secret_token = '60338052fdf39f7afcab6f9bdab04f54825ab24ee9eca60bf399e07392cd0bbe983d16a30e05792746050b6db50089660c5b5a0c8b305c6441f649b759175b4d'
