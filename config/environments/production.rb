ef5fa5901c03870496e34784d6e505a7b20f6c52
Jake Siddall <jacobsiddall@gmail.com>
2013-01-28 16:44:32 -0600

    Add email -specific to Gmail- for live test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ NoMercyChess::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'NoMercyChess.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
