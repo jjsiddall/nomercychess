aecaf2fca2d3584a8bfa897818cf87945f50c7c7
Siddall, Jake(jsiddall) <Jake.Siddall@cmegroup.com>
2014-04-04 23:03:59 -0500

    continuing to fix up the app so I can deploy Heroku...its a mess right now, but I'll get there

--- db/migrate/20130214221632_add_next_exercise_and_sort_for_lesson.rb
+++ db/migrate/20130214221632_add_next_exercise_and_sort_for_lesson.rb
@@ -1,4 +1,4 @@
-class Addnext_exerciseAndsort_for_lesson < ActiveRecord::Migration
+class AddNextExerciseAndSortForLesson < ActiveRecord::Migration
   def change
     add_column :exercises, :nextExercise, :string
     add_column :exercises, :sortForLesson, :integer
